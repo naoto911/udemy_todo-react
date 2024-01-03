0766ce43899296c893bfb99d53a8aaa15fb4ef3a
Naoto Kaimoto <73768470+naoto911@users.noreply.github.com>
2024-01-03 07:59:41 +0000

    未完了のTODOをcomponent切り出し

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { IncompeleteTodo } from "./components/IncompleteTodo";
 import { InputTodo } from "./components/InputTodo";
 import "./styles.css";
 
@@ -49,20 +50,11 @@ export const Todo = () => {
         onChange={onChangeTodoText}
         onClick={onClickAdd}
       />
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => (
-            <li key={todo}>
-              <div className="list-row">
-                <p className="todo-item">{todo}</p>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <IncompeleteTodo
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
